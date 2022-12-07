2755710909b4cb691aef9da2a0ced756bfaeff3a
topchikua <52373889+topchikua@users.noreply.github.com>
2022-12-07 03:27:26 +0200

    Update homework1.py

--- lesson1_decorators/tasks/homework1.py
+++ lesson1_decorators/tasks/homework1.py
@@ -12,13 +12,9 @@ import tracemalloc
 
 from time import sleep
 
-"""1. Реалізувати LFU алгоритм для кешування. За базу берем існуючий декоратор.Написати для фетчування юерелів. Додати можливість указати максимум елементів в кеші.
-https://uk.wikipedia.org/wiki/%D0%9D%D0%B0%D0%B9%D0%BC%D0%B5%D0%BD%D1%88_%D0%A7%D0%B0%D1%81%D1%82%D0%BE_%D0%92%D0%B8%D0%BA%D0%BE%D1%80%D0%B8%D1%81%D1%82%D0%B0%D0%BD%D0%B8%D0%B9_LFU
 
-2. Створити декоратор для заміру пам'яті.
-
-3. Створити окремий гіт репозиторій для домашніх завдань (github).
-Окрема гілка, окрема папка. Пул ріквест на main."""
+# TODO: 1. Реалізувати LFU алгоритм для кешування. За базу берем існуючий декоратор.Написати для фетчування юерелів.
+#  Додати можливість указати максимум елементів в кеші.
 
 
 def cache(max_limit=64):
@@ -49,10 +45,3 @@ def fetch_url(url, first_n=100):
     """Fetch a given url"""
     res = requests.get(url)
     return res.content[:first_n] if first_n else res.content
-
-
-link = 'https://uk.wikipedia.org/wiki/%D0%9D%D0%B0%D0%B9%D0%BC%D0%B5%D0%BD%D1%88_%D0%A7%D0%B0%D1%81%D1%82%D0%BE_%D0%92%D0%B8%D0%BA%D0%BE%D1%80%D0%B8%D1%81%D1%82%D0%B0%D0%BD%D0%B8%D0%B9_LFU'
-print(fetch_url(link))
-
-print('System memory:', psutil.virtual_memory())
-print(psutil.virtual_memory().active)
