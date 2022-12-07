57f1a2a13839098fbf3def067644f34f4be36fd4
topchikua <52373889+topchikua@users.noreply.github.com>
2022-12-07 06:09:21 +0200

    Decorators
    Homework tasks

---
+++ lesson1_decorators/Homework/homework2.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+import tracemalloc
+
+
+# TODO: 2. Створити декоратор для заміру пам'яті.
+
+
+def memory_check(msg='Повідомлення не отримано, дефолтний текст...'):
+    print(msg)
+
+    def outer(func):
+        def wrapper(*args, **kwargs):
+            tracemalloc.start()
+            result = func(*args, **kwargs)
+            first_size, first_peak = tracemalloc.get_traced_memory()
+            print(f"{datetime.now()}\nПри виконанні функції: {func.__name__}, \nбуде використано {first_size}B\n")
+            tracemalloc.reset_peak()
+            # print(result)
+            return result
+
+        return wrapper
+
+    return outer
+
+
+@memory_check(msg='Пам\'ять необхідна для обчислень:')
+def mylist(n):
+    result = [x for x in range(n) if x % 2 == 0]
+    return result
+
+
+mylist(1)
+mylist(111)
+mylist(65536)
